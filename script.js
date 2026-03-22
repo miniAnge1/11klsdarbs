ec6d2546afa6693049c67fc4f5c5eb176aefcfd1
miniAnge1 <demonmusi@gmail.com>
2026-03-22 22:53:47 +0200

    we oushing throuh errors

--- script.js
+++ script.js
@@ -2,46 +2,59 @@ let vielas = [];
 let inventars = [];
 
 async function start() {
-
+  try {
     let v = await fetch("vielas.json");
     let i = await fetch("inventars.json");
+
+    if (!v.ok || !i.ok) {
+      throw new Error("Neizdevās ielādēt JSON failus");
+    }
+
+    vielas = await v.json();
     inventars = await i.json();
 
     radit([...vielas, ...inventars]);
+
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function radit(dati){
+function radit(dati) {
+  let tabula = document.getElementById("tabula");
 
-    let tabula = document.getElementById("tabula");
-    tabula.innerHTML = "";
+  if (!tabula) {
+    console.error("Nav atrasts elements ar id 'tabula'");
+    return;
+  }
 
-    dati.forEach(x => {
+  tabula.innerHTML = "";
 
-        tabula.innerHTML += `
-        <tr>
-        <td>${x.id}</td>
-        <td>${x.nosaukums}</td>
-        <td>${x.skaits}</td>
-        </tr>
-        `;
-    });
+  dati.forEach(x => {
+    tabula.innerHTML += `
+      <tr>
+        <td>${x.id ?? ""}</td>
+        <td>${x.nosaukums ?? ""}</td>
+        <td>${x.skaits ?? ""}</td>
+      </tr>
+    `;
+  });
 }
 
-function filtrs(){
+function filtrs() {
+  let f = document.getElementById("filtrs").value;
 
-    let f = document.getElementById("filtrs").value;
-
-    if(f === "vielas"){
-        radit(vielas);
-    }
-
-    else if(f === "inventars"){
-        radit(inventars);
-    }
-
-    else{
-        radit([...vielas, ...inventars]);
-    }
+  if (f === "vielas") {
+    radit(vielas);
+  } 
+  else if (f === "inventars") {
+    radit(inventars);
+  } 
+  else {
+    radit([...vielas, ...inventars]);
+  }
 }
 
-start();
+let visiDati = [];
+let aktivaisFiltrs = "visi";
+;
